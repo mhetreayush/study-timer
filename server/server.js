7d40ccebea2eb96f1ba9ab61ae4240ba92fe59f5
Ayush <mhetreayush1719@gmail.com>
2022-10-25 12:09:21 +0530

    deployment stuff

--- server/server.js
+++ server/server.js
@@ -5,20 +5,13 @@ const cors = require("cors");
 const userRouter = require("./routes/userRouter");
 const studyTimerRouter = require("./routes/studyTimerRouter");
 const app = express();
+const path = require("path");
 app.use(express.json());
 app.use(cors());
 
 //routes
 app.use("/user", userRouter);
 app.use("/api/studyTimer", studyTimerRouter);
-
-//listen Server
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 //connect to MongoDB
 const URI = process.env.MONGODB_URL;
 mongoose.connect(
@@ -32,3 +25,17 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+//listen Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
