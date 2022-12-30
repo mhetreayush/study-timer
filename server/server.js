d97e19bac20e0269b25938e35fdff4de2e27c2d8
Ayush <mhetreayush1719@gmail.com>
2022-12-30 10:56:11 +0530

    process env port

--- server/server.js
+++ server/server.js
@@ -41,6 +41,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+app.listen(process.env.PORT);
